23165a8d84de334c278111dd4be9082712a87f5b
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2023-11-22 21:56:33 -0500

    got data saving to local storage

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ var currentDate = dayjs().format('dddd, MMMM DD');
 var todaysDateDisplay = document.getElementById("currentDay");
 var timeBlocks = document.getElementsByClassName('time-block');
 var saveButton = document.getElementsByClassName('btn');
+
 //added current date to header
 todaysDateDisplay.textContent = currentDate;
 
 $(function () {
 //wrote a function to cycle through the timeblocks on the page and determine their relation to the current time and console log it
 //updated function to add data to class lists to display proper css styling based on time of day
+console.log(currentTime);
   for (var i = 0; i < timeBlocks.length; i++) {
     var timeBlock = timeBlocks[i];
 
@@ -32,16 +34,15 @@ $(function () {
   $(saveButton).on("click", function() {
     var blockTime = $(this).closest('.time-block');
     var blockTimeID = blockTime.attr('id');
-    console.log(blockTimeID);
+    
+    // Updated event listener to save user input and respective time block to local storage
+    var userInput = blockTime.find('.userText');
+    var inputValue = userInput.val();
+    
+    console.log(inputValue);
+    localStorage.setItem(blockTimeID, inputValue);
   });
 
-  // DO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
